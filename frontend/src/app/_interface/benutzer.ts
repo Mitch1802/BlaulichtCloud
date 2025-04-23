2b40ae3dafe6533e75b7c0b8ffd13f3f8623a9b0
Michael Reichenauer <m.r.96@gmx.net>
2025-04-23 16:42:58 +0200

    Update benutzer.ts

--- frontend/src/app/_interface/benutzer.ts
+++ frontend/src/app/_interface/benutzer.ts
@@ -5,6 +5,6 @@ export interface IBenutzer {
     first_name: string,
     last_name: string,
     is_active: boolean,
-    is_staff: boolean,
-    password: string
+    password: string,
+    roles: any
 }
