112e1ec5c86c8366ff11cfc1c5984aa50c7f85eb
Mitch1802 <m.r.96@gmx.net>
2025-04-27 13:31:19 +0200

    Update views.py

--- django/core_apps/modul_konfiguration/views.py
+++ django/core_apps/modul_konfiguration/views.py
@@ -13,7 +13,10 @@ from core_apps.users.serializers import RoleSerializer
 class ModulKonfigurationListCreateView(generics.ListCreateAPIView):
     queryset = ModulKonfiguration.objects.all()
     serializer_class = ModulKonfigurationSerializer
-    permission_classes = [permissions.IsAuthenticated, HasAnyRolePermission.with_roles("ADMIN")]
+    permission_classes = [
+        permissions.IsAuthenticated,
+        HasAnyRolePermission.with_roles("ADMIN")
+    ]
     renderer_classes = [ModulKonfigurationenJSONRenderer]
 
     def list(self, request):
@@ -30,8 +33,12 @@ class ModulKonfigurationListCreateView(generics.ListCreateAPIView):
 class ModulKonfigurationRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = ModulKonfiguration.objects.all()
     serializer_class = ModulKonfigurationSerializer
-    permission_classes = [permissions.IsAuthenticated, HasAnyRolePermission.with_roles("ADMIN")]
-    lookup_field = "id"
+    permission_classes = [
+        permissions.IsAuthenticated,
+        HasAnyRolePermission.with_roles("ADMIN")
+    ]
+    lookup_field = 'module'
+    lookup_url_kwarg = 'module_key'
     renderer_classes = [ModulKonfigurationJSONRenderer]
 
     def retrieve(self, request, *args, **kwargs):
@@ -41,7 +48,29 @@ class ModulKonfigurationRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroy
             return Response(status=status.HTTP_404_NOT_FOUND)
 
         serializer = self.get_serializer(instance)
-
         return Response({
             'modul-konfiguration': serializer.data
         })
+
+    def update(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+        except Http404:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        partial = kwargs.pop('partial', False)
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({
+            'modul-konfiguration': serializer.data
+        })
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+        except Http404:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
