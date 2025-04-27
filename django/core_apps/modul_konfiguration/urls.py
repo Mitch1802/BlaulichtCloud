378ab7309210ccb9f182f242ea95d81c9e76541a
Mitch1802 <m.r.96@gmx.net>
2025-04-27 18:26:27 +0200

    Update urls.py

--- django/core_apps/modul_konfiguration/urls.py
+++ django/core_apps/modul_konfiguration/urls.py
@@ -3,16 +3,10 @@ from django.urls import path
 from .views import ModulKonfigurationListCreateView, ModulKonfigurationRetrieveUpdateDestroyView
 
 urlpatterns = [
-    # path("", ModulKonfigurationListCreateView.as_view(), name="modul-konfiguration-list-create"),
-    # path(
-    #     "<uuid:id>/",
-    #     ModulKonfigurationRetrieveUpdateDestroyView.as_view(),
-    #     name="modul-konfiguration-retrieve-update-destroy",
-    # ),
     path("", ModulKonfigurationListCreateView.as_view(), name="modul-konfiguration-list-create"),
     path(
-        "<str:modul_key>/", 
-        ModulKonfigurationRetrieveUpdateDestroyView.as_view(), 
-        name="modul-konfiguration-retrieve-update-destroy"
+        "<uuid:id>/",
+        ModulKonfigurationRetrieveUpdateDestroyView.as_view(),
+        name="modul-konfiguration-retrieve-update-destroy",
     ),
 ]
