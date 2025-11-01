42c8c4d8835f25ae0da623bda3799e2ce9442e7f
Mitch1802 <m.r.96@gmx.net>
2025-11-01 21:45:37 +0100

    feat(django): fix django urls (#11)

--- django/core_apps/atemschutz_masken/urls.py
+++ django/core_apps/atemschutz_masken/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import AtemschutzMaskenViewSet, AtemschutzMaskenProtokollViewSet
 
 router = DefaultRouter()
-router.register(r"masken", AtemschutzMaskenViewSet, basename='atemschutz-masken')
 router.register(r"masken/protokoll", AtemschutzMaskenProtokollViewSet, basename='atemschutz-masken-protokoll')
+router.register(r"masken", AtemschutzMaskenViewSet, basename='atemschutz-masken')
 
 urlpatterns = [
     path("", include(router.urls)),
