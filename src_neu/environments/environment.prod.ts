941bbea12512fc86af1aa539817362fb41d5b9c4
Michael Reichenauer <m.r.96@gmx.net>
2025-11-05 19:10:51 +0100

    feat(ui): clean folder

--- src_neu/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-import packageJson from '../../package.json';
-
-export const environment = {
-  production: true,
-  version: packageJson.version,
-  apiUrl: 'https://' + window.location.host + '/api/v1/'
-};
