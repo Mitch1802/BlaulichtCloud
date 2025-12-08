be1ece026ff3355d43a09f0e7e9674b9f33da817
Mitch1802 <m.r.96@gmx.net>
2025-12-09 00:12:55 +0100

    feat(django): load mitglieder list by default (#12)

--- django/core_apps/atemschutz_geraete/views.py
+++ django/core_apps/atemschutz_geraete/views.py
@@ -1,12 +1,14 @@
 from rest_framework import permissions, filters
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import AtemschutzGeraet, AtemschutzGeraetProtokoll
 from .serializers import AtemschutzGeraetSerializer, AtemschutzGeraetProtokollSerializer
 from core_apps.common.permissions import HasAnyRolePermission
-
+from core_apps.mitglieder.models import Mitglied
+from core_apps.mitglieder.serializers import MitgliedSerializer
     
 class AtemschutzGeraeteViewSet(ModelViewSet):
     queryset = AtemschutzGeraet.objects.all().order_by("inv_nr")
@@ -18,6 +20,11 @@ class AtemschutzGeraeteViewSet(ModelViewSet):
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ["inv_nr", "art", "typ"]
     ordering = ["inv_nr", "art", "typ"]
+
+    def list(self, request, *args, **kwargs):
+        resp = super().list(request, *args, **kwargs)
+        mitglieder = MitgliedSerializer(Mitglied.objects.all(), many=True).data
+        return Response({"main": resp.data, "mitglieder": mitglieder})
 
 class AtemschutzGeraeteProtokollViewSet(ModelViewSet):
     queryset = AtemschutzGeraetProtokoll.objects.all().order_by("datum")
