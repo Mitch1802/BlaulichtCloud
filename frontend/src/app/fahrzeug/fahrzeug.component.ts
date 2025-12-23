7c3da1685ddb0721afd80bf6c66863208eed0725
Mitch1802 <m.r.96@gmx.net>
2025-12-23 02:31:13 +0100

    feat(ui): weitere module angelegt

---
+++ frontend/src/app/fahrzeug/fahrzeug.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fahrzeug',
+  imports: [],
+  templateUrl: './fahrzeug.component.html',
+  styleUrl: './fahrzeug.component.sass'
+})
+export class FahrzeugComponent {
+
+}
---
+++ frontend/src/app/verwaltung/verwaltung.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verwaltung',
+  imports: [],
+  templateUrl: './verwaltung.component.html',
+  styleUrl: './verwaltung.component.sass'
+})
+export class VerwaltungComponent {
+
+}
