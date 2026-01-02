ad1146709681e570edee0940d3be36c2d34cef94
Mitch1802 <m.r.96@gmx.net>
2026-01-02 01:40:07 +0100

    fix(django): add default to model konfiguration

--- django/core_apps/konfiguration/models.py
+++ django/core_apps/konfiguration/models.py
@@ -5,18 +5,18 @@ from core_apps.common.models import TimeStampedModel
 
 
 class Konfiguration(TimeStampedModel):
-    fw_nummer = models.CharField(verbose_name=_("Feuerwehr Nummer"), max_length=255, unique=True)
-    fw_name =  models.CharField(verbose_name=_("Feuerwehr Name"), max_length=255)
-    fw_street =  models.CharField(verbose_name=_("Feuerwehr Straße"), max_length=255)
-    fw_plz =  models.CharField(verbose_name=_("Feuerwehr PLZ"), max_length=255)
-    fw_ort =  models.CharField(verbose_name=_("Feuerwehr Ort"), max_length=255)
-    fw_email =  models.CharField(verbose_name=_("Feuerwehr Email"), max_length=255)
-    fw_telefon =  models.CharField(verbose_name=_("Feuerwehr Telefon"), max_length=255)
-    fw_kdt =  models.CharField(verbose_name=_("Feuerwehr Kommandant"), max_length=255)
-    fw_webseite =  models.CharField(verbose_name=_("Feuerwehr Webseite"), max_length=255)
-    fw_konto =  models.CharField(verbose_name=_("Feuerwehr Konto"), max_length=255)
-    fw_iban =  models.CharField(verbose_name=_("Feuerwehr IBAN"), max_length=255)
-    fw_bic =  models.CharField(verbose_name=_("Feuerwehr BIC"), max_length=255)
+    fw_nummer = models.CharField(verbose_name=_("Feuerwehr Nummer"), max_length=255, unique=True, default="")
+    fw_name =  models.CharField(verbose_name=_("Feuerwehr Name"), max_length=255, default="")
+    fw_street =  models.CharField(verbose_name=_("Feuerwehr Straße"), max_length=255, default="")
+    fw_plz =  models.CharField(verbose_name=_("Feuerwehr PLZ"), max_length=255, default="")
+    fw_ort =  models.CharField(verbose_name=_("Feuerwehr Ort"), max_length=255, default="")
+    fw_email =  models.CharField(verbose_name=_("Feuerwehr Email"), max_length=255, default="")
+    fw_telefon =  models.CharField(verbose_name=_("Feuerwehr Telefon"), max_length=255, default="")
+    fw_kdt =  models.CharField(verbose_name=_("Feuerwehr Kommandant"), max_length=255, default="")
+    fw_webseite =  models.CharField(verbose_name=_("Feuerwehr Webseite"), max_length=255, default="")
+    fw_konto =  models.CharField(verbose_name=_("Feuerwehr Konto"), max_length=255, default="")
+    fw_iban =  models.CharField(verbose_name=_("Feuerwehr IBAN"), max_length=255, default="")
+    fw_bic =  models.CharField(verbose_name=_("Feuerwehr BIC"), max_length=255, default="")
 
     def __str__(self):
         return f"{self.fw_name}"
