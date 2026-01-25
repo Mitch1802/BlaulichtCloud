73e0fa4a67d8c42f6a6210631fa8af479f66c3c5
Mitch1802 <m.r.96@gmx.net>
2026-01-25 23:20:55 +0100

    feat(django): test add sevdeskmodul in url (#23)

--- django/core_apps/verwaltung/views.py
+++ django/core_apps/verwaltung/views.py
@@ -21,27 +21,29 @@ class VerwaltungGetView(APIView):
 
         # --- sevDesk (optional via Queryparam aktivieren) ---
         include_sevdesk = request.query_params.get("includeSevdesk", "0") == "1"
-        sevdesk_contacts = None
+        sevdesk_modul = request.query_params.get("sevdeskModul", "basics")
+        sevdesk_objects = None
         sevdesk_error = None
 
         if include_sevdesk:
             token = os.getenv("SEVDESK_API_TOKEN", "422acb071f99fefd3e2c74d787fdfd98")
             base_url = os.getenv("SEVDESK_BASE_URL", "https://my.sevdesk.de/api/v1")
 
+            if sevdesk_modul == "basics": sevdesk_modul= "Tools/bookkeepingSystemVersion"
+
             if not token:
                 sevdesk_error = "SEVDESK_API_TOKEN missing"
             else:
                 try:
                     r = requests.get(
-                        f"{base_url}/ContactAddress",
+                        f"{base_url}/{sevdesk_modul}",
                         headers={"Authorization": token},
                         timeout=20,
                     )
 
                     if r.status_code == 200:
                         data = r.json()
-                        # sevDesk liefert oft {"objects":[...], ...}
-                        sevdesk_contacts = data.get("objects", data)
+                        sevdesk_objects = data.get("objects", data)
                     else:
                         sevdesk_error = f"sevDesk request failed ({r.status_code})"
                 except requests.RequestException as e:
@@ -51,7 +53,7 @@ class VerwaltungGetView(APIView):
             "modul_konfig": modul_konfig,
             "konfig": konfig,
             "sevdesk": {
-                "contacts": sevdesk_contacts,
+                "objects": sevdesk_objects,
                 "error": sevdesk_error,
             } if include_sevdesk else None
         }
